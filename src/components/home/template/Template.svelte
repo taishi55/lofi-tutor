3dc960118d8adf69d1595e58f5e1adfba283faa3
taishi55 <105188885+taishi55@users.noreply.github.com>
2023-06-04 01:07:44 -0700

    prevent hide template

--- src/components/home/template/Template.svelte
+++ src/components/home/template/Template.svelte
@@ -14,7 +14,7 @@
   use:clickOutside
   transition:fly={{ duration: 200, y: 30, easing: cubicInOut }}
   on:click_outside={closeWindow}
-  class="common-bg common-border w-80 px-3 py-2 text-sm"
+  class="common-bg common-border w-80 z-50 px-3 py-2 text-sm"
 >
   {#if $templatePage === templatePages.listing}
     <TemplateList {closeWindow} />
