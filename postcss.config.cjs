3e753054fa9fd681ff368b82c3e834bd800fc8f5
taishi55 <105188885+taishi55@users.noreply.github.com>
2023-06-20 17:48:22 -0700

    fix nested css

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,9 @@
-// postcss.config.js (CommonJS format)
+// postcss.config.js
 module.exports = {
   plugins: {
+    'postcss-import': {},
+    'tailwindcss/nesting': {},
     tailwindcss: {},
     autoprefixer: {},
-  },
-};
+  }
+}
