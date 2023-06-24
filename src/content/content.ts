17d2ac46e210bafddb498ff105640629c002df94
taishi55 <105188885+taishi55@users.noreply.github.com>
2023-06-24 15:52:51 -0700

    smaller toggle btn

--- src/content/content.ts
+++ src/content/content.ts
@@ -82,7 +82,7 @@ function createSidebar() {
   sidebarTriggerButtonContainer.classList.add("sidebar-container");
 
   const sidebarTriggerButton = document.createElement("div");
-  sidebarTriggerButton.innerHTML = "Phantom";
+  sidebarTriggerButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#ffffff}</style><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>`;
   sidebarTriggerButton.id = "beyond_gafa_trigger";
   sidebarTriggerButton.classList.add("sidebar-trigger-button");
   sidebarTriggerButton.addEventListener("click", toggleSidebar);
